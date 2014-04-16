2c6931538caaf83e37774941b8d5ae2c9ca2f7f7
Denis Bodor <lefinnois@lefinnois.net>
2014-04-16 11:54:35 +0200

    enable fb console in kernel cmdline

--- u-boot/include/configs/stm32429-disco.h
+++ u-boot/include/configs/stm32429-disco.h
@@ -256,6 +256,7 @@
 #define CONFIG_BOOTDELAY		5
 #define CONFIG_HOSTNAME			stm32429-disco
 #define CONFIG_BOOTARGS			"stm32_platform=stm32429-disco mem=7M "\
+								"console=tty0 "\
 								"console=ttyS0,115200n8 consoleblank=0 "\
 								"root=/dev/mtdblock0 rdinit=/sbin/init "\
 								"video=vfb:enable,fbmem:0x90700000,fbsize:0x100000"
